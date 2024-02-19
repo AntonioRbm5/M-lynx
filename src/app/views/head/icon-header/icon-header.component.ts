dc9bbffc6269cc8066f990d9104353c9b68865eb
AntonioRbm5 <antoniorbm5@gmail.com>
2024-02-19 15:25:06 +0300

    configuration: mise en place de la structure de navigation

---
+++ src/app/views/head/icon-header/icon-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-header.component.html',
+  styleUrl: './icon-header.component.css'
+})
+export class IconHeaderComponent {
+
+}
