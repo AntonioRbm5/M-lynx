9883f9c7858e3b7cb42d51064c8daca83b9814bf
Tsilax <tsilla2020@gmail.com>
2024-03-18 12:58:31 +0300

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { CandidatureComponent } from './views/components/card/candidature.component';
+import { ListeCandidatureComponent } from './views/components/card/liste-candidature.component';
+import { ProfilCandidatComponent } from './views/components/card/profil-candidat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'user', children : [
+            { 
+                path : 'candidat', component : CandidatureComponent 
+            },
+            { 
+                path : 'liste', component : ListeCandidatureComponent 
+            },
+            {
+                path : 'profil', component : ProfilCandidatComponent
+            }
+        ]
+    },
+];
